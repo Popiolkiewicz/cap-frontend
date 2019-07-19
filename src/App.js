7cc098ea4bd2b39b7f9b1af8e2b6a49fe5711049
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2019-07-19 15:58:29 +0200

    #: Some basic nav pages + routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "./Routes";
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+
+                {/* Header */}
                 <Navbar bg="light" expand="lg" >
                     <Navbar.Brand href="/home"> Central Administration Panel</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/login">Login</Nav.Link>
-                            <Nav.Link href="/register">Register</Nav.Link>
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav>
+                            <LinkContainer to="/login">
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/register">
+                                <Nav.Link>Register</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
+
+                {/* Actual content based on URI */}
+                <Routes/>
+
+                {/* Footer */}
+
             </div>
         );
     }
---
+++ src/containers/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Register.css';
+
+class Register extends Component {
+
+    render() {
+        return (
+            <div className="Register">
+                Welcome to <h1>Registration page</h1>
+            </div>
+        );
+    }
+};
+
+export default Register;
